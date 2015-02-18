6865f5bf97610f4ddcf5950edd0ef60a54d20f61
Andreas Kautzsch <kautzsch@adesso.de>
2015-02-18 16:43:03 +0100

    version moved to root directory

---
+++ target/scala-2.11/src_managed/main/controllers/routes.java
@@ -0,0 +1,23 @@
+// @SOURCE:/Users/anka/Documents/@work/Projekte/Weiterbildung/continuous-delivery/with-play-and-scala/github-repo/ContDeliveryWithPlayScala/scala-act-app/conf/routes
+// @HASH:4f41fe2b6f59f0d1d4aec1fd7f4615d1d3133677
+// @DATE:Wed Feb 18 11:25:21 CET 2015
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+          
+}
+          
